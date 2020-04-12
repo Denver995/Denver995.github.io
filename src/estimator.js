64a50b4ce99af539643f6efaebafc0e11ea2ae3b
Denver995 <denverclaude99@gmail.com>
2020-04-12 23:52:22 +0100

    feat: edit code style

--- src/estimator.js
+++ src/estimator.js
@@ -1,4 +1,4 @@
-//funtion that estimate the infection By RequestedTime
+//	funtion that estimate the infection By RequestedTime
 const infectionsByRequestedTimeFunction = (periodType, currentlyInfected, timeToElapse) => {
 	let time = 0;
 	let result = 0;
@@ -46,25 +46,25 @@ const covid19ImpactEstimator = (data) => {
 	const input = data;
 	const impact = {};
 	const severeImpact = {};
-	//starting challenge 1
-	//estimation of currentlyInfected
+	//	starting challenge 1
+	//	estimation of currentlyInfected
 	impact.currentlyInfected = Math.trunc(input.reportedCases * 10);
 	severeImpact.currentlyInfected = Math.trunc(input.reportedCases * 50);
 
-	//estimation of infectionsByRequestedTime
+	//	estimation of infectionsByRequestedTime
 	impact.infectionsByRequestedTime = infectionsByRequestedTimeFunction(input.periodType, impact.currentlyInfected, input.timeToElapse);
 	severeImpact.infectionsByRequestedTime = infectionsByRequestedTimeFunction(input.periodType, severeImpact.currentlyInfected, input.timeToElapse);
 
-	//starting challenge 2
-	//estimation of severeCasesByRequestedTime
+	//	starting challenge 2
+	//	estimation of severeCasesByRequestedTime
 	impact.severeCasesByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.15);
 	severeImpact.severeCasesByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime * 0.15);
 
-	//estimation of hospitalBedsByRequestedTime
+	//	estimation of hospitalBedsByRequestedTime
 	impact.hospitalBedsByRequestedTime = Math.trunc(( input.totalHospitalBeds * 0.35 ) - impact.severeCasesByRequestedTime);
 	severeImpact.hospitalBedsByRequestedTime = Math.trunc(( input.totalHospitalBeds * 0.35 ) - severeImpact.severeCasesByRequestedTime);
 
-	//starting challenge 3
+	//	starting challenge 3
 	impact.casesForICUByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.05);
 	severeImpact.casesForICUByRequestedTime = Math.trunc(severeImpact.infectionsByRequestedTime * 0.05);
 
